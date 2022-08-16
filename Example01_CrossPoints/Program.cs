1f496989cb0bd8be15f1547972613cf29badfd45
VaulinDI <vdi85@mail.ru>
2022-08-16 19:36:26 +0300

    crosspoints task solved

---
+++ Example01_CrossPoints/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.WriteLine("Введите коэффициенты b1, k1 уравнения первой прямой и коэффициенты b2, k2 уравнения второй прямой: ");
+Console.Write("b1 = ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1 = ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2 = ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2 = ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точка пересечения двух данных прямых имеет координаты {Math.Round(x, 2)} {Math.Round(y, 2)}");
