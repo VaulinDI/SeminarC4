b0c2fcd9fbcec6f0a40a1a3ad38fc6744ca6de7a
VaulinDI <vdi85@mail.ru>
2022-08-19 20:24:16 +0300

    arraycopy task solved

---
+++ Example03_ArrayCopy/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу копирования массива
+int[] arr = {1, 4, 6, 5, 11};
+int[] copy = arr;
+
+void ArrayCopy(int[] array)
+{
+int n = arr.Length;    
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+}
+
+ArrayCopy(arr);
+Console.WriteLine();
+ArrayCopy(copy);
